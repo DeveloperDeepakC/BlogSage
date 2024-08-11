cd622e3f333bdeae7522a3e41265130b621e3f7c
DeveloperDeepakC <deepak.c767@gmail.com>
2024-08-11 12:37:54 +0530

    removed getName() from appbar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -11,9 +11,6 @@ export const Appbar = () => {
         navigate("/");
     }
 
-    function getName(){
-        
-    }
 
     return <div className="border-b flex justify-between px-10 py-4">
         <Link to={'/blogs'} className="flex flex-col justify-center cursor-pointer">
